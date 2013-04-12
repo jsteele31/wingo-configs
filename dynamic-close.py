bdd257418ab88ae59e91bff2070330d2e7c075f7
jsteele31 <jakejamessteele@gmail.com>
2013-04-11 17:49:06 -0700

    Update dynamic-close.py

--- dynamic-close.py
+++ dynamic-close.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python2
-
+ 
 import pywingo
-
-x = pywingo.Wingo()
-
-#Alright hopefully this makes an array lmao
-
-workspaceList = x.gribble('GetWorkspaceList')
-
-#split function
-
-workspaces = workspaceList.split()
-
-#Kill Function
-
-for workspace in workspaces:
-	result = x.gribble('GetClientList "%s"' % workspace)
-	if not result:
-		if workspace != 'trm':
-			x.gribble('RemoveWorkspace "%s"' % workspace)
+import time
+ 
+# Minimum Number of Workspaces (Prevents Uneccessary Errors)
+minWorkspaces = 1
+ 
+def main():
+ 
+    # Open Wingo socket
+    w = pywingo.Wingo()
+ 
+    # Close Active Client
+    w.gribble ('Close (GetActive)')
+ 
+    # Get a list of all available workspaces
+    workspaceList = w.gribble('GetWorkspaceList').split()
+ 
+    # Get a list of all clients in visible workspace
+    clientList = w.gribble('GetClientList (GetWorkspace)').split()
+ 
+ 
+    # If only one client was present upon closing
+    # and there is more workspaces the minimum allowed,
+    # then remove the current workspace
+    if len(clientList) == 1 and len(workspaceList) >= minWorkspaces:
+        time.sleep(0.1) # NOTE: delay is necessary...why?
+        w.gribble('RemoveWorkspace (GetWorkspace)')
+ 
+if __name__ == '__main__':
+    main()
